8649b90423d7d8184042f7b198467fe447e73a5e
sschoe85 <s@bine-schoetensack.eu>
2020-07-28 16:23:38 +0200

    First exports from button.js

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,25 +1,10 @@
 import "./button.css";
+import { addButton, addClickEvent } from "./button";
 //import button from "./button.html";
 
 export default { title: "Button" };
-const addButton = (text) =>{
-    const button=document.createElement('button');
-    button.classList.add("btn"); // der Button hat jetzt die Klasse btn
-    button.innerText = text;
-    return button;
-
- }
-
- const addClickEvent = (element) => {
-     
-     element.addEventListener("click", () => {
-         alert("button was clicked!");
-     })
-     return element;
- }
-
 export const addToCart = () => {
     const button = addButton("Test");
     
-    return addClickEvent(button); //Storybook ruft hier document.body.append(button) auf
+    return addClickEvent(button); 
 };
